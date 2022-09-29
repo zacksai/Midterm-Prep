07a980f5e354b22a8af32bebab2a00a2711e11e6
Zack Sai <zackjsai@gmail.com>
2022-09-29 00:17:49 -0700

    initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+
+    int a = -124;
+    double b = 12.43;
+    char temp = 'a';
+    bool c = 12341;
+
+    cin >> a;
+
+    cout << "a: " << a << "\nb: " << b <<"\ntemp: " << temp <<"\nc: " << c << endl;
+
+
+
+}
